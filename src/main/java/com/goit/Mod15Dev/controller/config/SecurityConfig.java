2be5576ab9141c2568cf4ef98b1ff5cbdc3f9e79
BobrovskaOlena <lena69114@gmail.com>
2023-08-13 22:20:22 +0300

    11

--- src/main/java/com/goit/Mod15Dev/controller/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.goit.Mod15Dev.controller.config;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-}
